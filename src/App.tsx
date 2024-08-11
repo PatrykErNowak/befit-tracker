2d7593f6720de09b88796265f421093d38dd006c
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-08-11 15:54:07 +0200

    Fix small UI Bug in AppLayout caused by comma

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
       {
         element: (
           <Suspense fallback={<LoadingPage />}>
-            <AuthLayout />,
+            <AuthLayout />
           </Suspense>
         ),
         children: [
